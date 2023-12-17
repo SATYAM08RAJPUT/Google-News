97cfc95aef512701e3cebd0286f6e1b33d8942c4
SATYAM08RAJPUT <satyamrajput081998@gmail.com>
2023-12-17 13:01:02 -0500

    change ui

--- header.js
+++ header.js
@@ -9,25 +9,47 @@ export default class Header {
     renderData(){
         const tpl =  `
             <div class = "subhead">
+            <div class ="left">
             <img src = "https://upload.wikimedia.org/wikipedia/commons/3/33/Google_News.png?20211029151451" class = "logo">
-            <input type = "search" placeholder="Searching a News & Sources" id ="serchinput">
-            <i class="fa fa-question-circle" style="font-size:24px"></i>
+            </div>
+            <div class ="middle">
+            <input type = "search" placeholder="Search for topics a News & Sources" id ="serchinput">
+            <i style="font-size:24px" class="fa">&#xf29c;</i>
             <i style="font-size:24px" class="fa"></i>
+            </div>
+            <div class ="right">
             <i style="font-size:24px" class="fa">&#xf2a1;</i>
-            <img src ="https://png.pngtree.com/thumb_back/fh260/background/20220713/pngtree-login-icon-steel-protection-login-photo-image_8659186.jpg" class ="login">
+            <img src ="https://lh3.googleusercontent.com//J6_coFbogxhRI9iM864NL_liGXvsQp2AupsKei7z0cNNfDvGUmWUy20nuUhkREQyrpY4bEeIBuc=rj-w300-h300-l95-c0xffffff" class ="login">
+            </div>
             </div>`
         document.getElementById("headercontainer").innerHTML = tpl;
     }
 
     renerNews(data){
-        const add = `<div class="topheading">Top Stories > </div>`
+        const add = `<div class="topheading">Top Stories 
+        <i style="font-size:30px" class="fa">&#xf105;</i>
+        </div>
+        <div class ="line"></div>`
         const html = data.map((item) => {
-            return `<div class ="seeui">
+            return `<div class ="render">
+            <div class="renderleft">
             <img src ="${item.urlToImage}" class ="imgrender">
-            <h3>${item.title}</h3>
-            <h4>${item.publishedAt}</h4>
-            <h5>${item.location}</h5>
-            <p class ="para"><b>Description:-</b>${item.description}</p>
+            <p>${item.title}</p>
+            <div class ="publice">${item.publishedAt}</div>
+            <div class ="location">${item.location}</div>
+            </div>
+            <div class ="renderright">
+            <img src ="https://www.exchange4media.com/news-photo/93037-IndianExpressflame.jpg" class ="express">
+            <div class ="para" id ="news">${item.News1}</div>
+            <p class ="para" id ="des">${item.description}</p>
+            <img src ="https://assets-global.website-files.com/6191dc5130687d707cbc32f6/62fcd88c93bd3f81dd887b38_Hindustan%20Times_banner.svg" class ="hindutanNews">
+            <div class ="para" id ="news">${item.News2}</div>
+            <p class ="para" id ="des">${item.descriptionsecond}</p>
+            <img src ="https://www.newspaperadsbooking.in/images/newspaper-logo/Times-of-India-logo-b.jpg" class ="timeofNews">
+            <div class ="para"id ="news" >${item.News3}</div>
+            <p class ="para" id ="des">${item.descriptionthird}</p>
+            <div>
+            <div class ="linesecond"></div>
             </div>`
         }).join(" ")
         document.getElementById('rendernews').innerHTML = add + html;
